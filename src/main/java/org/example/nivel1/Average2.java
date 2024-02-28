c510ad55cb4fe91e7ee97726baeeedd63d45944b
cavlima <mathews.clima@gmail.com>
2024-02-28 11:09:14 -0300

    Exercicios

--- src/main/java/org/example/nivel1/Average2.java
+++ src/main/java/org/example/nivel1/Average2.java
@@ -1,10 +1,14 @@
 package org.example.nivel1;
 
+import java.util.Scanner;
+
 public class Average2 {
 
     public static void main(String[] args) {
 
     int a,b;
+
+    Scanner sc = new Scanner(System.in);
 
 
 
