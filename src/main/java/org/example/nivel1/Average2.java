326a956d75ec170c8547804a8c26d3739940791e
cavlima <mathews.clima@gmail.com>
2024-03-12 15:19:29 -0300

    Att Averag2 e Difference

--- src/main/java/org/example/nivel1/Average2.java
+++ src/main/java/org/example/nivel1/Average2.java
@@ -7,7 +7,7 @@ public class Average2 {
 
         Scanner scanner = new Scanner(System.in);
 
-        /*double a = scanner.nextDouble();
+        /*double a = scanner.cnextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
 
@@ -15,29 +15,34 @@ public class Average2 {
         System.out.println(String.format("MEDIA = %.1f",media));
         scanner.close();*/
 
+
         averageWithValidator(scanner);
     }
 
     private static void averageWithValidator(Scanner scanner) {
+
         double[] numeros = new double[3];
         double[] pesos = {2, 3, 5};
         double somaPesos = 0, somaNotas = 0;
 
-        for (int i = 0; i < numeros.length; i++) {
+        // Loop para ler as 3 notas
+        for (int i = 0; i < numeros.length; i++) { //loop para ler as 3 notas
 
+            // Loop infinito para solicitar ao usuário que insira uma nota válida
             while (true) {
+                // Verifica se a próxima entrada é um número decimal válido
                 if (scanner.hasNextDouble()) {
-                    numeros[i] = scanner.nextDouble();
-                    somaNotas += numeros[i] * pesos[i];
-                    somaPesos += pesos[i];
-                    break;
+                    numeros[i] = scanner.nextDouble(); // leitura da entrada como uma nota
+                    somaNotas += numeros[i] * pesos[i]; // calcula a soma ponderada das 3 notas
+                    somaPesos += pesos[i]; // calcula a soma dos pesos
+                    break; // encerra o loop while
                 } else {
                     System.out.println("Entrada inválida. Por favor, insira um número válido.");
                     scanner.next();
                 }
-            } // fim do while
-
+            } // fim do while e volta para o loop for
         } // fim do for
+
         scanner.close();
         double media = somaNotas / somaPesos;
         System.out.printf("MEDIA = %.1f%n", media);
---
+++ src/main/java/org/example/nivel1/Difference.java
@@ -0,0 +1,32 @@
+package org.example.nivel1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Difference {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        /*int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        int dif = ((a*b) - (c*d));
+        System.out.println(String.format("DIFERENCA = %d",dif));*/
+
+        System.out.println("DIFERENCA = " + diferenca(scanner));
+
+    }
+
+    private static int diferenca(Scanner scanner) {
+        int[] numbers = new int[4];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        return ((numbers[0]*numbers[1]) - (numbers[2]*numbers[3]));
+    }
+}
